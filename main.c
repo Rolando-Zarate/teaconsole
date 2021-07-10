940e758dbfdd64ebd38029a56071cd560c3aae47
Rolando-Zarate <73555986+Rolando-Zarate@users.noreply.github.com>
2021-07-10 01:45:04 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#define MAX_INPUT_SIZE 256
+
+int main(){
+    printf("Tea Console\n");
+    while (true){
+        char *inp = malloc(MAX_INPUT_SIZE);
+        if (inp == NULL){
+            printf("No available memory, Exiting program.");
+            getchar();
+            return -1;
+        }
+        printf(">>>");
+        fgets(inp,MAX_INPUT_SIZE,stdin);
+        if ((strlen(inp) > 0) && (inp[strlen(inp) -1] == '\n')){
+            inp[strlen(inp) -1]  = '\0';
+        }
+        system(inp);
+        free(inp);
+    }
+    return 0;
+}
